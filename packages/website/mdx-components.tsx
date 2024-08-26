f78b01461df585f8a42fb62867693445a54e0958
Adam Mikacich <adammikacich@gmail.com>
2024-08-25 23:27:04 -0700

    add basic docs

--- packages/website/mdx-components.tsx
+++ packages/website/mdx-components.tsx
@@ -1,9 +1,12 @@
-import type { MDXComponents } from 'mdx/types';
+import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
 import defaultComponents from 'fumadocs-ui/mdx';
+import type { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
-    ...components
+    ...components,
+    Tab,
+    Tabs
   };
 }
