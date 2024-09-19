0ef0f9993ff059a09f63cef4d7bc959fbead75fd
Adam Mikacich <adammikacich@gmail.com>
2024-09-19 16:59:55 +0200

    add route creator

--- packages/api/src/wrappers/routes.ts
+++ packages/api/src/wrappers/routes.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { ZodType } from 'zod';
+import { z, ZodType } from 'zod';
 import { ApiResponse } from '../types';
 import { execute } from './common';
 
@@ -15,4 +15,59 @@ export function wrapRoute<InputType, OutputType>(
     const apiResponse = await execute(inputSchema, outputSchema, fn, inputData);
     return NextResponse.json(apiResponse, { status: apiResponse.status });
   };
+}
+
+class RouteCreator<InputType = unknown, OutputType = unknown> {
+  inputSchema?: ZodType<InputType>;
+  outputSchema?: ZodType<OutputType>;
+  handlerFunc?: (input: InputType) => Promise<OutputType>;
+
+  constructor() {
+    this.inputSchema = z.any();
+    this.outputSchema = z.any();
+    this.handlerFunc = undefined;
+  }
+
+  input<T>(inputSchema: ZodType<T>): RouteCreator<T, OutputType> {
+    const creator = new RouteCreator<T, OutputType>();
+    creator.inputSchema = inputSchema;
+    creator.outputSchema = this.outputSchema;
+    creator.handlerFunc = this.handlerFunc as
+      | ((input: T) => Promise<OutputType>)
+      | undefined;
+    return creator;
+  }
+
+  output<T>(outputSchema: ZodType<T>): RouteCreator<InputType, T> {
+    const creator = new RouteCreator<InputType, T>();
+    creator.inputSchema = this.inputSchema;
+    creator.outputSchema = outputSchema;
+    creator.handlerFunc = this.handlerFunc as
+      | ((input: InputType) => Promise<T>)
+      | undefined;
+    return creator;
+  }
+
+  handler(
+    handlerFunc: (input: InputType) => Promise<OutputType>
+  ): (request: NextRequest) => Promise<NextResponse<ApiResponse<OutputType>>> {
+    if (!this.inputSchema || !this.outputSchema) {
+      throw new Error('Input and output schemas must be defined');
+    }
+
+    return wrapRoute<InputType, OutputType>(
+      this.inputSchema,
+      this.outputSchema,
+      handlerFunc
+    );
+  }
+}
+
+type RouteCreatorReducedScope = Pick<
+  RouteCreator,
+  'handler' | 'input' | 'output'
+>;
+
+export function createRoute(): RouteCreatorReducedScope {
+  return new RouteCreator();
 }
